184724b3812697ab9be58319c42a6d5a0ae286e5
Seokjin Han <raifthenerd@gmail.com>
2016-09-16 21:55:24 +0900

    rssi to distance (with naive filtering)

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from config import LOCATIONS
+from config import LOCATIONS, TXPOWER
+from math import sqrt, pow
 from subprocess import Popen, PIPE
 
 
-RSSI = {beacon: None for beacon in LOCATIONS}
+def distance(rssi):
+    return sqrt(pow(10, (TXPOWER - rssi) / 10))
 
 with Popen(['node', 'location.js'], stdout=PIPE) as scan:
+    distances = {beacon: [] for beacon in LOCATIONS}
     while True:
         addr, rssi = scan.stdout.readline().split()
         addr = addr.decode()
-        if addr in RSSI.keys():
-            RSSI[addr] = int(rssi)
-        if all(RSSI.values()):
-            print(RSSI)  # TODO
+        if addr in distances.keys():
+            if len(distances[addr]) == 10:
+                distances[addr].pop(0)
+            distances[addr].append(int(rssi))
+        if all(map(lambda x: len(x) == 10, distances.values())):
+            foo = {x: distance(sum(distances[x])/10) for x in distances.keys()}
+            print(foo)  # TODO
