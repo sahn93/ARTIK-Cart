b7399856c233e4f46208da419a89c9b1864eb79b
Seokjin Han <raifthenerd@gmail.com>
2016-09-16 20:57:49 +0900

    add config file

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+from config import LOCATIONS
 from subprocess import Popen, PIPE
-
-
-LOCATIONS = {'88:c2:55:10:f7:38': None,
-             '88:c2:55:10:31:17': None,
-             '88:c2:55:11:10:1b': None,
-             '88:c2:55:11:10:2e': None}
 
 
 RSSI = {beacon: None for beacon in LOCATIONS}
