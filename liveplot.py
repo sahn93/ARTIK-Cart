3426e7c3a7abd96e7833815e56ac0063a3454b86
Seokjin Han <raifthenerd@gmail.com>
2016-10-09 15:14:27 +0900

    draw a plot on laptop

--- liveplot.py
+++ liveplot.py
@@ -1,30 +1,50 @@
 #!/usr/bin/python3
 
+import sys
+import signal
+import socket
+import pickle
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
+host = '10.42.0.1'
+port = 53516
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(True)
+conn, addr = s.accept()
+
+w = open('./data.csv', 'w')
+w.write('x, y, t\n')
+
+def safe_close(signal=None, frame=None):
+    print('GOOD BYE!')
+    w.close()
+    sys.exit(0)
+signal.signal(signal.SIGTERM, safe_close)
+signal.signal(signal.SIGINT, safe_close)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 ax1.set_xlim([0,300])
 ax1.set_ylim([0,300])
 
+xs = []
+ys = []
 
 def animate(i):
-	graph_data = open('coordinates.csv','r').read()
-	lines = graph_data.split('\n')
-	lines = lines[1:]
-	xs = []
-	ys = []
-	for line in lines:
-		if len(line) > 1:
-			x, y, t = line.split(',')
-			xs.append(x)
-			ys.append(y)
-	ax1.clear()
-	ax1.set_xlim([0,300])
-	ax1.set_ylim([0,300])
-	ax1.plot(xs, ys)
+    data = conn.recv(4096)
+    x, y, t = pickle.loads(data)
+    print((x, y, t))
+    w.write("%f, %f, %f\n" % (x, y, t))
+    xs.append(x)
+    ys.append(y)
+    ax1.clear()
+    ax1.set_xlim([0,300])
+    ax1.set_ylim([0,300])
+    ax1.plot(xs, ys)
 
 ani = animation.FuncAnimation(fig, animate, interval = 100)
 plt.show()
+conn.close()
--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import sys
 import math
 import time
 import RTIMU
+import socket
 import signal
-import subprocess
+import pickle
 from config import CALIB_FILE, LOCATIONS, TXPOWER
 from math import pi, sqrt, pow, degrees
 
 def distance(rssi):
     return sqrt(pow(10, (TXPOWER - rssi) / 10))
+
+host = os.environ['HOST']
+port = 53516
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((host, port))
+print('Connected to the server')
 
 s = RTIMU.Settings(CALIB_FILE)
 imu = RTIMU.RTIMU(s)
@@ -35,30 +42,33 @@ with open('/sys/class/gpio/export', 'w') as f:
 with open('/sys/class/gpio/gpio%d/direction' % pin, 'w') as f:
     f.write('in')
 
-(x, y, w_prev, theta_prev, theta_curr) = (150, 150, 0, 0, 0)
+def close_socket(signal=None, frame=None):
+    print('GOOD BYE!')
+    server.close()
+    sys.exit(0)
+signal.signal(signal.SIGTERM, close_socket)
+signal.signal(signal.SIGINT, close_socket)
+
+(x, y, w_prev, theta_prev, theta_curr) = (0, 150, 0, 0, 0)
 s_prev = False
 dt = 0.01
-with open('./coordinates.csv', 'w') as ff:
-    ff.write("x(cm), y(cm), time_interval(s)\n")
-    tstart = time.time()
-    subprocess.Popen(["./liveplot.py"])
-    while True:
-        time.sleep(t_interval)
-        if imu.IMURead():
-            data = imu.getIMUData()
-            w_curr = data['gyro'][2]
-            theta_curr += 0.5*dt*(w_prev + w_curr)
-            w_prev = w_curr
-        with open('/sys/class/gpio/gpio%d/value' % pin, 'rb', 0) as f:
-            s_curr = f.read() == b'0\n'
-        if s_prev is not s_curr:
-            s_prev = s_curr
-            if not s_prev:
-                theta = 0.5*(theta_prev + theta_curr)
-                x += pi*diameter*math.cos(theta)
-                y += pi*diameter*math.sin(theta)
-                theta_prev = theta_curr
-                print("%.1fcm, %.1fcm, %.1fs" % (x, y, time.time() - tstart))
-                ff.write("%.1f, %.1f, %.1f\n" % (x, y, time.time() - tstart))
-                tstart = time.time()
-                ff.flush()
+tstart = time.time()
+while True:
+    time.sleep(t_interval)
+    if imu.IMURead():
+        data = imu.getIMUData()
+        w_curr = data['gyro'][2]
+        theta_curr += 0.5*dt*(w_prev + w_curr)
+        w_prev = w_curr
+    with open('/sys/class/gpio/gpio%d/value' % pin, 'rb', 0) as f:
+        s_curr = f.read() == b'0\n'
+    if s_prev is not s_curr:
+        s_prev = s_curr
+        if not s_prev:
+            theta = 0.5*(theta_prev + theta_curr)
+            x += pi*diameter*math.cos(theta)
+            y += pi*diameter*math.sin(theta)
+            theta_prev = theta_curr
+            print((x, y, time.time() - tstart))
+            server.send(pickle.dumps((x, y, time.time() - tstart)))
+            tstart = time.time()
